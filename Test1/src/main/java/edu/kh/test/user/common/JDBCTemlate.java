2c2848aee51a8f085e67c1eb7a114d29ca30ecec
chichi980911 <shkim3657@nate.com>
2023-08-25 00:02:03 +0900

    commit

--- Test1/src/main/java/edu/kh/test/user/common/JDBCTemlate.java
+++ Test1/src/main/java/edu/kh/test/user/common/JDBCTemlate.java
@@ -1,70 +1,67 @@
 package edu.kh.test.user.common;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Properties;
 
 public class JDBCTemlate {
-	
-	
-	//1.커넥션
 	public static Connection getConnection() {
+		Connection conn = null;
 		
-		Connection conn = null; //3
+		Properties prop = new Properties();
 		
-		//1.클래스 파일로 등록
+		String filepath = JDBCTemlate.class.getResource("db/driver.properies").getPath();
+	
+		try {
+			prop.load(new FileInputStream(filepath));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");   //1
-			
-			//2. 커넥션 객체 생성 (url,계정명, 비번) //DriverManager.getConnection
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "TEST1", "TEST1");  //2
-			
+			Class.forName(prop.getProperty("driver"));
+			conn = DriverManager.getConnection(prop.getProperty("url"),
+												prop.getProperty("username"),
+												prop.getProperty("password")
+								);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return conn;
-		
-		
-	
-		
-	}
-	
-	
-	//2. 반납 (커넥션,stmt,rset)
-	
-	public static void close(Connection conn) {
-		try {
-			if(conn != null && !conn.isClosed()) {
-				conn.close();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void close(Statement stmt) {
-		try {
-			if(stmt != null && !stmt.isClosed()) {
-				stmt.close();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void close(ResultSet rset) {
-		try {
-			if(rset != null && !rset.isClosed()) {
-				rset.close();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
 
+		return conn;
+		}
+	
+	
+	
+
+public static void commit(Connection conn) {
+	try {
+		if(conn != null && conn.isClosed()) {
+			conn.commit();
+		}
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 }
+	
+public static void rollbakc(Connection conn) {
+	try {
+		if(conn != null && conn.isClosed()) {
+			conn.rollback();
+		}
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+	
+}
+
